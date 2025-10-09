db7b00ec2d3393af41626957108e4d180e6d36f2
Emily DuBois <emilymdubois@gmail.com>
2025-10-09 16:26:58 -0500

    build: configure output dir and basic styling setup (#3)
    - Changed output directory from `_site` to `public`
    - Added `src` directory and moved `index.md` into it
    - Created base HTML and CSS for home page
    - Added CSS as passthrough and watch target in 11ty config

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,3 +1,16 @@
 export default async function(eleventyConfig) {
-	eleventyConfig.addPassthroughCopy("CNAME");
+  // Copy unprocessed source files to public directory.
+	eleventyConfig.addPassthroughCopy("./CNAME");
+  eleventyConfig.addPassthroughCopy("./src/css");
+
+  // Hot reload when the style changes.
+  eleventyConfig.addWatchTarget("./src/css");
+
+  // Return config object.
+  return {
+    dir: {
+      input: "src",
+      output: "public"
+    }
+  }
 };
